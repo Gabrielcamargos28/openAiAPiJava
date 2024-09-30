07c88378001509ed3353456b3ed14df3108c3108
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2024-09-29 21:19:22 -0300

    att

--- src/main/java/org/com/firstech/Main.java
+++ src/main/java/org/com/firstech/Main.java
@@ -9,22 +9,38 @@ import com.theokanning.openai.service.OpenAiService;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        String userPrompt = "Escova de dentes";
-        String systemMessage = "Você é um gerador de produtos de um ecommerce de produtos digitais e deve responder apenas a categoria";
+        var  input = new Scanner(System.in);
 
+        while(true){
+            System.out.println("Digite a entrada relacionada as categorias: ");
 
+            var categorias = input.nextLine();
 
+            System.out.println("Digite a entrada relacionada ao nome: ");
+
+            var nomeProduto = input.nextLine();
+
+            String userPrompt = "Escova de dentes";
+            String systemMessage = "" +
+                    "Você é um gerador de produtos de um ecommerce de produtos digitais e deve responder apenas a categoria" +
+                    "%s".formatted(categorias);
+
+            dispararRequisicao(userPrompt, systemMessage);
+        }
+    }
+
+    public static void dispararRequisicao(String user, String system) {
         String apiKey = System.getenv("OPENAI_API_KEY");
 
         if (apiKey == null) {
             System.err.println("API key not found. Set the OPENAI_API_KEY environment variable.");
             return;
         }
-
 
         OpenAiService service = new OpenAiService(apiKey, Duration.ofSeconds(30));
 
@@ -33,8 +49,8 @@ public class Main {
                 .builder()
                 .model("gpt-4")
                 .messages(Arrays.asList(
-                        new ChatMessage(ChatMessageRole.USER.value(), userPrompt),
-                        new ChatMessage(ChatMessageRole.SYSTEM.value(), systemMessage)
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
                 ))
                 .n(5)
                 .build();
