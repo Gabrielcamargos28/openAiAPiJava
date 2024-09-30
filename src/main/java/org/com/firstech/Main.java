a74ccf1b8b1cce54228660f35410380718f40d11
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2024-09-29 21:08:20 -0300

    added params

--- src/main/java/org/com/firstech/Main.java
+++ src/main/java/org/com/firstech/Main.java
@@ -6,14 +6,16 @@ import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        String userPrompt = "Gere 5 produtos";
-        String systemMessage = "Você é um gerador de produtos de um ecommerce de produtos digitais";
+        String userPrompt = "Escova de dentes";
+        String systemMessage = "Você é um gerador de produtos de um ecommerce de produtos digitais e deve responder apenas a categoria";
+
 
 
         String apiKey = System.getenv("OPENAI_API_KEY");
@@ -24,7 +26,7 @@ public class Main {
         }
 
 
-        OpenAiService service = new OpenAiService(apiKey);
+        OpenAiService service = new OpenAiService(apiKey, Duration.ofSeconds(30));
 
 
         ChatCompletionRequest completionRequest = ChatCompletionRequest
@@ -34,6 +36,7 @@ public class Main {
                         new ChatMessage(ChatMessageRole.USER.value(), userPrompt),
                         new ChatMessage(ChatMessageRole.SYSTEM.value(), systemMessage)
                 ))
+                .n(5)
                 .build();
 
         try {
